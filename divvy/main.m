086e142510ea99a242f7c7234befaa8044f74800
Nikhil Srinivasan <nikhil@nvs.io>
2013-10-06 00:07:59 -0400

    initial commit

---
+++ divvy/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  divvy
+//
+//  Created by Nikhil Srinivasan on 10/6/13.
+//  Copyright (c) 2013 Nikhil Srinivasan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "nvsAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([nvsAppDelegate class]));
+    }
+}
---
+++ divvy/nvsAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  nvsAppDelegate.h
+//  divvy
+//
+//  Created by Nikhil Srinivasan on 10/6/13.
+//  Copyright (c) 2013 Nikhil Srinivasan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface nvsAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
